a84490e588caa241eb1df6aaa455aa5814f1b2b5
Samuel <smarouba2@gmail.com>
2022-03-30 08:34:44 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance <= 400){
+  return'This one is on me!'
+  }else if (2000 < distance && distance < 2500){
+  return 'I will gladly take your thirty bucks.'
+  } else {
+      return'No can do.'
+  }
+}
+ 
+
+function ternaryCheckCity(destinationCity){
+  return (destinationCity === "NYC"? 'Ok, sounds good.': 'No go.');
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+ function switchOnCharmFromTip(tip){
+  switch(tip) {
+     case 'generous':
+      return 'Thank you so much.';
+     case 'not as generous':
+    return 'Thank you.';
+    case 'thanks for everything':
+    return 'Bye.';
+  }
 }
